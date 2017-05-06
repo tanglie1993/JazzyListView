5d0b86de537954b694d824775d54a49fe708ed2b
tanglie1993 <tanglie23@163.com>
2017-05-06 23:48:07 +0800

    移除魔术数字

--- app/src/main/java/com/tanglie1993/jazzylistview/CardAnimation.java
+++ app/src/main/java/com/tanglie1993/jazzylistview/CardAnimation.java
@@ -13,6 +13,9 @@ class CardAnimation extends Animation {
     private int height = 0;
     private int width = 0;
 
+    private static final float INITIAL_X_PADDING = 0.3f;
+    private static final float INITIAL_Y_RELATIVE_POSITION = 0.8f;
+
     @Override
     public void initialize(int width, int height, int parentWidth, int parentHeight) {
         super.initialize(width, height, parentWidth, parentHeight);
@@ -23,10 +26,10 @@ class CardAnimation extends Animation {
     @Override
     protected void applyTransformation(float interpolatedTime, Transformation t) {
         Matrix matrix = new Matrix();
-        float v1 = (float) (0.4 * interpolatedTime + 0.6);
-        float v3 = (float) 0.2 * width * (1 - interpolatedTime);
+        float v1 = 1 - 2 * INITIAL_X_PADDING * (1 - interpolatedTime);
+        float v3 = INITIAL_X_PADDING * width * (1 - interpolatedTime);
         float v5 = interpolatedTime;
-        float v6 = (float) 0.8 * height * (1 - interpolatedTime);
+        float v6 = INITIAL_Y_RELATIVE_POSITION * height * (1 - interpolatedTime);
         float v9 = 1;
         float[] values = {v1, 0, v3, 0, v5, v6, 0, 0, v9};
         matrix.setValues(values);
