5baf4d1068e319e6daaa13bb2c449f695cd05a6c
tanglie1993 <tanglie23@163.com>
2017-05-07 14:20:23 +0800

    修改动画效果

--- app/src/main/java/com/tanglie1993/jazzylistview/CardItemAnimator.java
+++ app/src/main/java/com/tanglie1993/jazzylistview/CardItemAnimator.java
@@ -1,5 +1,7 @@
 package com.tanglie1993.jazzylistview;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.ScaleAnimation;
@@ -10,14 +12,22 @@ import android.view.animation.ScaleAnimation;
 
 public class CardItemAnimator implements ListItemAnimator {
     @Override
-    public void animateItem(View view) {
+    public void animateItem(final View view) {
         if(view == null){
             return;
         }
-        CardAnimation animation = new CardAnimation();
-        animation.setDuration(1000);
-        animation.setFillAfter(false);
+        ValueAnimator animation = ObjectAnimator.ofFloat(0, 1);
+        animation.setDuration(400);
         animation.setInterpolator(new AccelerateDecelerateInterpolator());
-        view.startAnimation(animation);
+        animation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float animatedValue = (Float) animation.getAnimatedValue();
+                view.setRotationX((1 - animatedValue) * 90);
+                view.setTranslationY((float) (view.getMeasuredHeight() * 0.3 * (1 - animatedValue)));
+                view.setScaleX((float) (0.5 + 0.5 * animatedValue));
+            }
+        });
+        animation.start();
     }
 }
